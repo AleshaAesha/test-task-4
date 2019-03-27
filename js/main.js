ebb0674c790e99f0d2935c64b34f68740257a177
unknown <you@example.com>
2019-03-27 17:01:21 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -15,14 +15,19 @@ fg.src = "img/base.png";
 pipeUp.src = "img/pipe-green.png";
 pipeDown.src = "img/pipe-green-down.png";
 
+
 var gap = 90;
+var xPos = 150;
+
+
+var move = 1;
 
 //pipes
 
 var pipe = [];
 
 pipe[0] = {
-	x : canvas.wigth,
+	x : 200,
 	y : 0
 }
 
@@ -32,52 +37,46 @@ pipe[0] = {
 document.addEventListener("keydown", moveUp);
 
 function moveUp() {
-	yPos -= 20;
+	bY -= 20;
 }
 
 
 
 //bird position
 
-var xPos = 10;
-var yPos = 150;
-var move = 1;
+var bX = 10;
+var bY = 150;
+
+
 
 
 function draw() {
 	ctx.drawImage(bg, 0, 0);
-
+	
 	for(var i = 0; i < pipe.length; i++) {
 		ctx.drawImage(pipeDown, pipe[i].x, pipe[i].y);
-	// ctx.drawImage(pipeUp, 100, 0 pipeDown.height + gap);
+		ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y + pipeDown.height + gap);
 
-	pipe[i].x--;
-
-	if(pipe[i].x == 125) {
-		pipe.push({
-			x : cvs.wigth,
-			y : Math.floor(Math.random()* pipeDown.height) -
-			pipeDown.height
+		pipe[i].x--;
+		if(pipe[i].x == 100) {
+			pipe.push({
+				x : canvas.width,
+				y : Math.floor(Math.random()*pipeDown.height) - 
+				pipeDown.height
 			});
 		}
+	} 
 
-		if(xPos + bird.wigth >= pipe[i].x
-			&& xPos <= pipe[i].x + pipeDown.wigth
-			&& (yPos <= pipe[i].y + pipeDown.height
-				|| yPos + bird.height >= pipe[i].y + pipeUp.height + gap)
-					|| yPos + bird.height >= canvas.height - fg.height) {
-			location.reload();
-		}
-	}
 
 		
+
+	ctx.drawImage(fg, 0, bg.height - fg.height);
+	ctx.drawImage(bird, bX, bY);
+
+	bY += move;
+	requestAnimationFrame(draw);
 	
 
-	ctx.drawImage(fg, 0, canvas.height - fg.height);
-	ctx.drawImage(bird, xPos, yPos);
-
-	yPos += move;
-	requestAnimationFrame(draw);
 }
 
 pipeDown.onload = draw;
